4907934e247315f8f5be0356243a95e889c71f17
paulohaps <paulomdohenrique123@gmail.com>
2026-09-03 22:34:35 -0400

    Atualiza cache após migração para Neon

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = 'gastos-ape-v2';
+const CACHE_NAME = 'gastos-ape-v3';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
@@ -12,6 +13,14 @@ self.addEventListener('install', event => {
         './manifest.json'
       ]);
     })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 

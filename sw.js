5e9e3cbb8a155079ae7ae7a009d9787c58c17977
paulohaps <paulomdohenrique123@gmail.com>
2026-09-03 23:10:49 -0400

    Força atualização de JS e evita cache obsoleto

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = 'gastos-ape-v4';
+const CACHE_NAME = 'gastos-ape-v5';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './css/style.css',
-        './js/api.js',
-        './js/app.js',
-        './manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './css/style.css',
+      './manifest.json'
+    ]))
   );
 });
 
@@ -25,7 +21,23 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.endsWith('/js/api.js') || url.pathname.endsWith('/js/app.js')) {
+    event.respondWith(
+      fetch(event.request, { cache: 'no-store' })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
